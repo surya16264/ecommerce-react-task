0f9e07426e502e12a810560bb377542621ed7848
vishensiddharth <vishensiddharth01@yahoo.com>
2021-07-02 16:22:26 +0530

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h3>Lets start</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, quidem maiores officiis quod eaque temporibus provident placeat magni, repudiandae molestiae id repellendus quaerat facilis praesentium!</p>
     </div>
   );
 }
