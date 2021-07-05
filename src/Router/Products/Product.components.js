bbf92970408063d6b99cf1a925ff10da2ff70f91
surya16264 <iamsurya1626@gmail.com>
2021-07-05 16:52:32 +0530

    dummy change

--- src/Router/Products/Product.components.js
+++ src/Router/Products/Product.components.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Products extends Component {
     render() {
         return (
-            <h1>Product Page</h1>
+            <h1>Product</h1>
         )
     }
 }
